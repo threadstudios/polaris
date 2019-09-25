9f724834c045a58fdfc64594a22795298230a19a
Paul Westerdale <paul@westerdale.me>
2019-09-25 15:31:14 +0100

    initial commit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import "core-js/stable";
+import React from "react";
+import ReactDOM from "react-dom";
+
+window.React = React;
+window.ReactDOM = ReactDOM;
---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.js",
+  mode: "production",
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      }
+    ]
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "core.js"
+  }
+};
